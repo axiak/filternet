ca978edc65d2fd001d18872bc8f90151e3d8e7c6
Michael Axiak <mike@axiak.net>
2012-01-23 16:48:33 -0500

    Fix additional code in google.

--- node/nodeproxy.js
+++ node/nodeproxy.js
@@ -121,7 +121,7 @@ var serverDefinition = function (http_mod, default_port) { return catch_errors(f
           var originalLength = buffer.length;
           buffer = buffer.replace(/<\/body>/i, ADDITIONAL_CODE[default_port] + "</body>");
           if (buffer.length == originalLength && buffer.search(/<html>/i) !== -1) {
-              buffer += ADDITIONAL_CODE;
+              buffer += ADDITIONAL_CODE[default_port];
           }
           response.end(buffer);
       } else {
