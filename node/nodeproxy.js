44317acea4f0997c2310bb360ef21981c8c84ff3
Michael Axiak <mike@axiak.net>
2012-01-23 16:43:28 -0500

    Fixed ssl.

--- node/nodeproxy.js
+++ node/nodeproxy.js
@@ -1,11 +1,20 @@
 var http = require('http')
   , url = require('url')
   , fs = require('fs')
-  , https = require('https');
-var ADDITIONAL_CODE = "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>" +
-                      "<script type='text/javascript'>$.noConflict();</script>" +
-                      "<script type='text/javascript' src='http://cdnjs.cloudflare.com/ajax/libs/require.js/1.0.1/require.min.js'></script>" +
-                      "<script type='text/javascript' src='http://axiak.github.com/injectfun/index.js'></script>";
+  , https = require('https')
+  , net = require('net');
+
+var ADDITIONAL_CODE = {
+    80: "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>" +
+        "<script type='text/javascript'>$.noConflict();</script>" +
+        "<script type='text/javascript' src='http://cdnjs.cloudflare.com/ajax/libs/require.js/1.0.1/require.min.js'></script>" +
+        "<script type='text/javascript' src='http://axiak.github.com/injectfun/index.js'></script>",
+
+    443: "<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>" +
+        "<script type='text/javascript'>$.noConflict();</script>" +
+        "<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/require.js/1.0.1/require.min.js'></script>" +
+        "<script type='text/javascript' src='http://axiak.github.com/injectfun/index.js'></script>"
+};
 
 var PORT = process.env.PORT || 8000;
 
@@ -31,6 +40,7 @@ function fixHeaders(request, oldHeaders) {
       result[header] = oldHeaders[header.toLowerCase()];
     }());}
   }
+  delete result['Connection'];
   return result;
 }
 
@@ -71,7 +81,7 @@ var debug_view = function (request_info) {
 };
 
 
-var serverDefinition = function (http_mod) { return catch_errors(function(request, response) {
+var serverDefinition = function (http_mod, default_port) { return catch_errors(function(request, response) {
   request.headers['accept-encoding'] = 'identity';
   delete request.headers['accept-encoding'];
   delete request.headers['proxy-connection'];
@@ -79,13 +89,12 @@ var serverDefinition = function (http_mod) { return catch_errors(function(reques
   var parsed_url = url.parse(request.url);
 
   var request_info = {
-    'host': parsed_url.hostname
-  , 'port': ~~(parsed_url.port || 80)
+    'host': parsed_url.hostname || request.headers['host']
+  , 'port': ~~(parsed_url.port || default_port)
   , 'path': parsed_url.pathname + (parsed_url.search || '') + (parsed_url.hash || '')
   , 'method': request.method
   , 'headers': fixHeaders(request, request.headers)
   };
-
   debug_view(request_info);
 
   var proxy_request = http_mod.request(request_info, function (proxy_response) {
@@ -110,7 +119,7 @@ var serverDefinition = function (http_mod) { return catch_errors(function(reques
       if (isHtml) {
           delete proxy_response.headers['content-length'];
           var originalLength = buffer.length;
-          buffer = buffer.replace(/<\/body>/i, ADDITIONAL_CODE + "</body>");
+          buffer = buffer.replace(/<\/body>/i, ADDITIONAL_CODE[default_port] + "</body>");
           if (buffer.length == originalLength && buffer.search(/<html>/i) !== -1) {
               buffer += ADDITIONAL_CODE;
           }
@@ -138,7 +147,7 @@ var serverDefinition = function (http_mod) { return catch_errors(function(reques
 });
 };
 
-var server = http.createServer(serverDefinition(http));
+var server = http.createServer(serverDefinition(http, 80));
 server.listen(PORT);
 server.on('clientError', function (error) {
     console.log("ClientError failure.");
@@ -150,13 +159,41 @@ server.on('error', function (error) {
     console.log(error);
     console.log(error.stack);
 });
+server.on('upgrade', function (request, socket, head) {
+     var parsed_url = url.parse('https://' + request.url);
+     /*
+     var clientSocket = net.createConnection(~~(parsed_url.port || 443),
+                                             parsed_url.hostname);
+     */
+     var clientSocket = net.createConnection(PORT + 1,
+                                             'localhost');
+     clientSocket.on('connect', function () {
+         socket.write("200 OK\r\n\r\n");
+     });
+     clientSocket.on('data', function (data) {
+         socket.write(data);
+     });
+     clientSocket.on('end', function () {
+         socket.end();
+     });
+     socket.on('data', function (data) {
+         try {
+             clientSocket.write(data);
+         } catch (error) {
+             console.log(error.stack);
+         }
+     });
+     socket.on('end', function () {
+         clientSocket.end();
+     });
+});
 
 var sslOptions = {
     key: fs.readFileSync('keys/server.key'),
     cert: fs.readFileSync('keys/server.crt')
 };
 
-var httpsServer = https.createServer(sslOptions, serverDefinition(https));
+var httpsServer = https.createServer(sslOptions, serverDefinition(https, 443));
 httpsServer.listen(PORT + 1);
 httpsServer.on('clientError', function (error) {
     console.log("ClientError failure.");
