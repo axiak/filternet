acbc8bb930f3cbd506cf0bffd92ed5b4c0b11d46
Michael Axiak <mike@axiak.net>
2012-01-28 00:03:40 -0500

    Added deadline.

--- node/nodeproxy.js
+++ node/nodeproxy.js
@@ -16,8 +16,7 @@ var ADDITIONAL_CODE = {
 };
 
 var isEnabled = function () {
-//    var enabled = new Date().getTime() > new Date(2012, 1, 9, 12, 30).getTime();
-    var enabled = new Date().getTime() > new Date(2012, 0, 9, 12, 30).getTime();
+    var enabled = new Date().getTime() > new Date(2012, 1, 9, 8, 30).getTime();
     return enabled;
 };
 
